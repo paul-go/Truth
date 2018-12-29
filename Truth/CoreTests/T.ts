ef8b9dccf18d5ab889be6dd9122aa7940aa6cecd
paulgo <paul@getblank.com>
2018-12-29 14:23:29 -0500

    Project organization

--- Truth/CoreTests/T.ts
+++ Truth/CoreTests/T.ts
@@ -1,4 +1,5 @@
 export * from "./Baselines/Acceptance";
+export * from "./Framework/BaselineTypes";
 export * from "./Framework/BaselineParser";
 export * from "./Framework/BaselineRunner";
 export * from "./Framework/TestSetup";
