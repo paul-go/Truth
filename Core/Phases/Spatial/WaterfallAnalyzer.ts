7d724e03aa59a3cbed12f788c6df815c5e696db7
paulgo <paul@getblank.com>
2019-01-03 11:25:46 -0500

    WIP (Restructuring waterfall rendering)

--- Core/Phases/Spatial/WaterfallAnalyzer.ts
+++ Core/Phases/Spatial/WaterfallAnalyzer.ts
@@ -2,9 +2,22 @@ import * as X from "../../X";
 
 
 /**
- * 
+ * @internal
+ * This is the main class that performs type verification.
+ * It analyzes a waterfall, produces a skip set (which is a
+ * Set<X.Fan> that indicates which fans were found to be
+ * type-level cruft). The skip set is fed to a WaterfallWalker
+ * instance.
  */
 export class WaterfallAnalyzer
 {
-	
+	/** */
+	static invoke(waterfall: X.Waterfall)
+	{
+		const skipSet = new Set<X.Fan>();
+		
+		
+		
+		return skipSet;
+	}
 }
