04731a37caf017b700fed7ab7e6f9ea34229eae6
paulgo <paul@getblank.com>
2019-01-16 13:46:45 -0500

    Style

--- Truth/Debug/Start.ts
+++ Truth/Debug/Start.ts
@@ -1,6 +1,6 @@
 import * as X from "../CoreTests/X";
-import * as Fs from "fs";
 import * as Viz from "./Viz";
+import * as Fs from "fs";
 
 
 /**
@@ -9,6 +9,7 @@ import * as Viz from "./Viz";
 setTimeout(() =>
 {
 	Viz.init();
+	
 	const fileContent = Fs.readFileSync(process.argv.slice(-1)[0], "utf8");
 	const program = new X.Program(false);
 	const doc = program.documents.create(fileContent);
@@ -19,11 +20,6 @@ setTimeout(() =>
 	];
 	const uri = X.Uri.create(doc.sourceUri.extend([], typePath));
 	const type = program.query(uri);
-	
-	if (!type)
-	{
-		//debugger;
-		return;
-	}
+	type;
 },
 0);
