c1135a756e03bec95acd0e6d090b53ba91b49b2a
paulgo <paul@getblank.com>
2019-01-17 08:33:25 -0500

    Adds a driver for running debug sessions

--- Debug/Start.ts
+++ Debug/Start.ts
@@ -10,16 +10,74 @@ setTimeout(() =>
 {
 	Viz.init();
 	
-	const fileContent = Fs.readFileSync(process.argv.slice(-1)[0], "utf8");
+	const findArg = (key: string) =>
+	{
+		const val = process.argv.find(arg => arg.startsWith(key)) || "";
+		return val.slice(key.length);
+	};
+	
+	const filePath = findArg("--file=");
+	const typePath = findArg("--typePath=");
+	const fileContent = Fs.readFileSync(filePath, "utf8");
 	const program = new X.Program(false);
 	const doc = program.documents.create(fileContent);
-	const typePath = [
-		"SubClass",
-		"Property",
-		"Max"
-	];
-	const uri = X.Uri.create(doc.sourceUri.extend([], typePath));
+	const uri = X.Uri.create(doc.sourceUri.extend([], typePath.split("/")));
 	const type = program.query(uri);
-	type;
+	
+	const uriMessage = `Using URI: ${filePath}//${typePath}`;
+	const pipe = "-".repeat(uriMessage.length);
+	console.log(pipe);
+	console.log(uriMessage);
+	console.log(pipe);
+	
+	if (type === null)
+	{
+		console.log(`No type exists at the input URI.`);
+		return;
+	}
+	
+	const printedFaults = new Set<X.Fault>();
+	const printFault = (fault: X.Fault) =>
+	{
+		if (printedFaults.has(fault))
+			return;
+		
+		printedFaults.add(fault);
+		const t = fault.type;
+		
+		const statement = X.Guard.notNull(
+			fault.source instanceof X.Statement ? fault.source :
+			fault.source instanceof X.Span ? fault.source.statement :
+			fault.source instanceof X.InfixSpan ? fault.source.statement :
+			null);
+		
+		const line = doc.getLineNumber(statement);
+		const causedBy = fault.source.constructor.name;
+		console.log(`\t${t.code}: ${t.message} on line ${line} (${causedBy})`);
+	}
+	
+	if (type.faults.length > 0)
+	{
+		console.log(`The following faults were detected at the input URI:`);
+		
+		for (const fault of type.faults)
+			printFault(fault);
+	}
+	else
+	{
+		console.log(`No faults were detected at the input URI.`);
+	}
+	
+	if (program.faults.count > 0)
+	{
+		console.log(`The following anciliary faults were also detected:`);
+		
+		for (const fault of program.faults.each())
+			printFault(fault);
+	}
+	else
+	{
+		console.log(`No anciliary faults were detected:`);
+	}
 },
-0);
+1);
