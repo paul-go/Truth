18867bef800a8f6f0c12ac35bfdb4950a269f571
paulgo <paul@getblank.com>
2019-02-14 12:31:21 -0500

    Adds support for minifying the build output

--- release.js
+++ release.js
@@ -6,6 +6,8 @@ const ReleaseDir = "./Build/Release/";
 const UnminifiedFile = ReleaseDir + "truth.js";
 const MinifiedFile = ReleaseDir + "truth.min.js";
 
+const Fs = require("fs");
+
 task("Compiling to temporary directory...", () =>
 {
 	exec("tsc -p ./tsconfig.release.json");
@@ -43,6 +45,7 @@ task("Bundling into a single file...", async () =>
 	const options = {
 		input: ReleaseDir + "Core/X.js",
 		output,
+		treeshake: false,
 		onwarn: warning =>
 		{
 			if (warning.code === "THIS_IS_UNDEFINED")
@@ -55,28 +58,88 @@ task("Bundling into a single file...", async () =>
 		}
 	};
 	
-	const rollupBuild = await rollup(options);
-	rollupBuild.write(output);
+	const built = await rollup(options);
+	await built.write(output);
+});
+
+task("Replacing debug constant", () =>
+{
+	const content = Fs.readFileSync(UnminifiedFile)
+		.toString("utf8")
+		// Replaces the debug string with a long number
+		// that is of an equivalent character length (to prevent
+		// source maps from getting messed up), but also
+		// results in a falsy value.
+		.replace(/"DEBUG"/gm, "0x00000");
+	
+	Fs.writeFileSync(UnminifiedFile, content);
 });
 
 // Disable for this until we can get this:
 // https://github.com/terser-js/terser
 // working
-0 && task("Minifying code...", () =>
+task("Minifying code...", () =>
 {
-	exec(`terser ${UnminifiedFile}
-		--output ${MinifiedFile}
-		--keep-classnames
-		--keep-fnames`);
+	const terser = require("terser");
+	const unminifiedSource = Fs.readFileSync(UnminifiedFile).toString("utf8");
+	
+	const terserResult = terser.minify(unminifiedSource, {
+		keep_classnames: true,
+		keep_fnames: true,
+		warnings: true,
+		sourceMap: {
+			url: "inline"
+		},
+		compress: {
+			unused: false
+		}
+	});
+	
+	if (terserResult.error)
+	{
+		console.error(terserResult.error);
+		return;
+	}
+	
+	if (terserResult.warnings)
+		for (const warning of terserResult.warnings)
+			console.log(warning);
+	
+	Fs.writeFileSync(MinifiedFile, terserResult.code);
 });
 
-async function task(message, taskFn)
-{
-	console.log(message);
-	await taskFn();
-}
+task("Done", () => {});
+
+
+//
+// Build script tools
+//
+
 
 function exec(cmd)
 {
 	require("child_process").execSync(cmd.replace(/[\r\n]/g, ""));
+}
+
+async function task(title, taskFn)
+{
+	const self = this;
+	self.entries = self.entries || [];
+	self.entries.push({ title, taskFn });
+	
+	if (self.hasInit)
+		return;
+	
+	self.hasInit = true;
+	setTimeout(async () =>
+	{
+		for (const entry of self.entries)
+		{
+			console.log(entry.title);
+			const result = entry.taskFn();
+			
+			if (result instanceof Promise)
+				await result;
+		}
+	}, 0);
 }
