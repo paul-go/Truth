af802078ec7d2183b467a3e76cfde48822027fd1
paulgo <paul@getblank.com>
2019-02-21 08:58:07 -0500

    Cruft removal

--- Quick/AgentSamples/Agent.truth.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../Build/Release/truth.d.ts" />
-/// <reference path="editor.d.ts" />
-
-program.on(Truth.CauseEditComplete, data =>
-{
-	console.log("edit complete from hook.");
-});
-
-
