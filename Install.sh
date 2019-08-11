1d71042b6dfa524ba66442a09209e992563ecf51
Parsa Ghadimi <me@qti3e.com>
2019-08-11 23:54:31 +0430

    Install.sh: Ask user about using sudo.

--- Install.sh
+++ Install.sh
@@ -10,9 +10,21 @@ cd ..
 ln -s ./Makejs/build/source/make.d.ts ./make.d.ts
 ln -s ./Makejs/build/source/make.js ./make.js
 
-echo "This script will globally install the latest versions of: TypeScript, Jest, Terser, Puppeteer."
+echo "This script will globally install the latest versions of: Jest, Terser, Puppeteer."
 read -p "Press any key to continue, or CTRL+C to quit."
+read -p "Do you want to use 'sudo' to install global packages? [y/n] " useSudoKey
 
-npm install -g jest
-npm install -g terser
-npm install -g puppeteer
+useSudo=0; [ $useSudoKey = "y" ] && useSudo=1
+
+npmInstallGlobal() {
+    if [ $useSudo = 1 ]
+    then
+        sudo npm install -g $1
+    else
+        npm install -g $1
+    fi
+}
+
+npmInstallGlobal jest
+npmInstallGlobal terser
+npmInstallGlobal puppeteer
