adaff698e962ead8105e20eb93ba12703cadc2c7
Parsa Ghadimi <me@qti3e.com>
2019-09-08 17:14:53 +0430

    Implement System

---
+++ Backer/Talk/source/System.ts
@@ -0,0 +1,59 @@
+import { Query } from "./Query";
+import * as Truth from "truth-compiler";
+
+/**
+ * Manages the global Truth document and context.
+ */
+export class System
+{
+  /**
+   * Singleton instance.
+   */
+  private static _this: System | undefined;
+
+  /**
+   * Singleton accessor property.
+   */
+  static this()
+  {
+    if (!System._this)
+      throw new Error("You must first initialize a TruthTalk system.");
+    return System._this;
+  }
+
+  /**
+   * Initializes the system from a Truth file.
+   */
+  static async fromFile(truthFilePath: string)
+  {
+		await new Promise(r => r());
+		const doc = await Truth.read(truthFilePath);
+		if (doc instanceof Error)
+			return doc;
+
+    System._this = new System(doc);
+  }
+
+  /**
+   * Initializes the system from a Truth text.
+   */
+	static async fromText(truthFileText: string)
+	{
+		await new Promise(r => r());
+		const doc = await Truth.parse(truthFileText);
+		if (doc instanceof Error)
+			return doc;
+		
+		System._this = new System(doc);
+	}
+
+  private constructor(readonly doc: Truth.Document) {}
+
+  /**
+   * Constructs a new query on the default document.
+   */
+  query()
+  {
+    return new Query(this.doc.types.slice());
+  }
+}
