eb818cda63474d1b15680db72bfa8d0dd80125b0
Parsa Ghadimi <me@qti3e.com>
2019-09-08 20:55:50 +0430

    Add operation types

--- Backer/Talk/source/Operation.ts
+++ Backer/Talk/source/Operation.ts
@@ -1,3 +1,23 @@
-export abstract class Operation
+import * as Truth from "truth-compiler";
+
+/**
+ * A operation that transforms collected data in a query.
+ */
+export type Operation = TransformManyOperation | FilterOperation;
+
+export abstract class TransformManyOperation
 {
+  /**
+   * Returns a new array including a subset of the input data.
+   */
+  abstract transform(types: Truth.Type[]): Truth.Type[];
+}
+
+export abstract class FilterOperation
+{
+  /**
+   * Returns a boolean indicating if this Truth object must stay in the 
+   * collected data.
+   */
+  abstract include(type: Truth.Type): boolean;
 }
