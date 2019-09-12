479c3dc05dd590b85aa396f7958bd92e7667c904
Parsa Ghadimi <me@qti3e.com>
2019-09-12 13:02:25 +0430

    Start porting the library to Reflex

---
+++ Backer/Talk/source/Library.ts
@@ -0,0 +1,68 @@
+/// <reference path="../../../Reflex/ReflexCore/build/source/reflex-core.d.ts" />
+
+namespace Reflex.Talk {
+	type Branch = never;
+	type Node = never;
+
+	class Library extends Core.Library 
+	{
+		constructor() 
+		{
+			super({});
+		}
+
+		isKnownBranch(branch: Core.IBranch) 
+		{
+			return true;
+		}
+
+		getNamespaceStatic() 
+		{
+			return {};
+		}
+
+		getNamespaceComputed() 
+		{
+			return () => Reflex.Core.ComputedMemberType.branch;
+		}
+
+		createBranch(name: string) 
+		{
+			return undefined as any;
+		}
+
+		getChildren(target: Branch) 
+		{
+			return [];
+		}
+
+		isBranchDisposed(branch: Branch) 
+		{
+			return null;
+		}
+
+		prepareContent(content: any) 
+		{
+			return null;
+		}
+
+		attachPrimitive(
+			primitive: any,
+			owner: Branch,
+			ref: Node | "prepend" | "append"
+		) {}
+
+		detachPrimitive(primitive: any, owner: Branch) {}
+
+		attachAttribute(branch: Branch, key: string, value: any) {}
+
+		detachAttribute(branch: Branch, key: string) {}
+
+		attachRecurrent() 
+		{
+			return false;
+		}
+
+		detachRecurrent() {}
+	}
+}
