6146bb7bed0bd7ffb7d14ec4a75966d37b870737
Kaan <kaaninel@gmail.com>
2019-09-29 11:47:58 +0300

    CLI Errors & cleanup example.code.json example desired format

--- Encoder/Projects/CLI/CLI.ts
+++ Encoder/Projects/CLI/CLI.ts
@@ -8,13 +8,26 @@ import PrimeType from "../Core/Type";
  */
 export default class JSONCLI 
 {
-	constructor(Config: EncoderConfig)
+	constructor(public Config: EncoderConfig)
 	{
-		const json = JSON.stringify(Config.Code, null, 2);
-		writeFileSync(Config.Raw.Declarations, json);
-		const array = JSON.parse(json);
-		console.log(array); // first output
-		console.log(array.map((x: [number] & any[]) => Serializer.decode(x, PrimeType.JSONLength))); // second output
+		Config.Scanner.document.program.verify();
+		const faults = Config.Scanner.document.program.faults;
+		if (faults.count)
+		{
+			for (const fault of faults.each())
+				console.error(fault.toString());
+		}
+		else
+		{	
+			this.save();
+		}
+	}
+	
+	save()
+	{
+		const json = JSON.stringify(this.Config.Code);
+		writeFileSync(this.Config.Raw.Declarations, json);
+		console.info(`Truth Code File: ${this.Config.Raw.Declarations} saved!`);
 	}
 }
 
