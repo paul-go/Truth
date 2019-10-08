8d4f8a153a3c524609098ca2f3bb38a532dc8240
paulgo <paul@truebase.com>
2019-10-08 11:39:27 -0400

    Truth: Add a make.ts script

---
+++ Truth/make.ts
@@ -0,0 +1,43 @@
+/// <reference types="makets" />
+
+make.on("bundle", "publish", async () =>
+{
+	const outJsFile = "bundle/truth.js";
+	
+	await make.typescript("source/tsconfig.json", {
+		compilerOptions: {
+			target: "es5",
+			stripInternal: true,
+			downlevelIteration: true,
+			outFile: "../" + outJsFile
+		}
+	});
+	
+	make.compilationConstants(outJsFile, {
+		DEBUG: false
+	});
+	
+	const returns = "Truth";
+	
+	make.definitions(
+		returns,
+		"bundle/truth.d.ts");
+	
+	make.augment(outJsFile, { returns });
+	make.minify(outJsFile);
+});
+
+make.on("publish", () =>
+{
+	make.publish({
+		packageFileChanges: {
+			name: "truth-compiler",
+			main: "./truth.js",
+			types: "./truth.d.ts",
+			jest: null,
+			scripts: null,
+			devDependencies: null,
+			eslintIgnore: null
+		}
+	});
+});
