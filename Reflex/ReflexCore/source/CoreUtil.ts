798890239a9fc91dd33afabd07054ca37d476cec
paulgo <paul@truebase.com>
2019-10-14 09:51:24 -0400

    Reflex: Code cleanup

--- Reflex/ReflexCore/source/CoreUtil.ts
+++ Reflex/ReflexCore/source/CoreUtil.ts
@@ -17,6 +17,7 @@ namespace Reflex.Core
 			containerMeta: ContainerMeta,
 			rawPrimitives: unknown)
 		{
+			const lib = RoutingLibrary.this;
 			const primitives = Array.isArray(rawPrimitives) ?
 				rawPrimitives.slice() :
 				[rawPrimitives];
@@ -37,7 +38,7 @@ namespace Reflex.Core
 				// strings, numbers, and bigints are passed through verbatim in this phase.
 				else if (typeof primitive !== "object")
 					continue;
-					
+				
 				else if (Array.isArray(primitive))
 					primitives.splice(i--, 1, ...primitive);
 				
@@ -105,7 +106,7 @@ namespace Reflex.Core
 						metas.push(existingMeta);
 					
 					else if (typeof primitive === "object" &&
-						RoutingLibrary.this.isKnownLeaf(primitive))
+						lib.isKnownLeaf(primitive))
 						metas.push(new InstanceMeta(primitive));
 					
 					// This error occurs when something was passed as a primitive 
@@ -248,7 +249,9 @@ namespace Reflex.Core
 					lib.attachAttribute(containingBranch, meta.key, meta.value);
 				}
 				
-				Buffer.add(containingBranch, meta);
+				if ("DEBUG")
+					Buffer.add(containingBranch, meta);
+				
 				tracker.update(meta.locator);
 			}
 		}
