a9df0476617c52d0b2ba008c2f53ce7c3a04b7fd
paulgo <paul@truebase.com>
2019-10-17 11:46:48 -0400

    TruthTalk: Fixes code ordering

---
+++ Backer/TruthTalk/source/@.ts
@@ -0,0 +1,4 @@
+/// <reference path="../../../Reflex/ReflexCore/build/reflex-core.d.ts" />
+/// <reference path="Definitions.ts" />
+/// <reference path="Node.ts" />
+/// <reference path="Library.ts" />
