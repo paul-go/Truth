dd17d1bcd8891435ca52ac2ad669614e045135ce
Kaan <kaaninel@gmail.com>
2019-10-21 22:00:08 +0300

    TruthTalk.Test: Integrate backer for testing

--- Backer/TruthTalk.Test/source/Start.ts
+++ Backer/TruthTalk.Test/source/Start.ts
@@ -1,9 +1,19 @@
 
 namespace Backer.TruthTalk.Test
 {
-	const ast = tt(
-		tt.is()
-	);
-	
-	debugger;
+	//> eqwe
+	(async function()
+	{
+		await Code.link("/Backer/TruthTalk.Test/code.json", "/Backer/TruthTalk.Test/Product.data.json");
+		
+		const {
+			Product
+		} = Schema;
+		
+		const ast = tt(
+			tt.is(),
+		);
+		console.log(ast);
+		Execute({}, ast);
+	})();	
 }
