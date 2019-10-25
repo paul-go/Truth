52a943f2e25f8e355288d382e09470359d5fb8e3
Kaan <kaaninel@gmail.com>
2019-10-25 05:03:18 +0300

    TT.Engine: Relocation works

--- Backer/TruthTalk.Engine/source/CursorSet.ts
+++ Backer/TruthTalk.Engine/source/CursorSet.ts
@@ -3,16 +3,16 @@ namespace Backer.TruthTalk
 {
 	export class CursorSet
 	{
-		cursors: PLATypes[];
+		cursors: Set<PLATypes>;
 		
 		constructor(...cursors: PLATypes[])
 		{
-			this.cursors = cursors;
+			this.cursors = new Set(cursors);
 		}
 		
 		snapshot()
 		{
-			return this.cursors.slice();
+			return Array.from(this.cursors);
 		}
 		
 		clone()
@@ -22,24 +22,25 @@ namespace Backer.TruthTalk
 		
 		filter(fn: (v: PLATypes) => boolean)
 		{
-			this.cursors = this.cursors.filter(x => fn(x));
+			this.cursors = new Set(this.snapshot().filter(x => fn(x)));
 		}
 		
-		query(ast: Branch | Leaf, not = false) 
+		query(ast: Branch | Leaf) 
 		{
 			if (ast instanceof Branch)
-				this.branch(ast, not);
+				this.branch(ast);
 			else 
-				this.leaf(ast, not);
+				this.leaf(ast);
 		}
 		
-		branch(branch: Branch, not = false) 
+		branch(branch: Branch) 
 		{
 			switch (branch.op)
 			{
+				case BranchOp.is:
 				case BranchOp.query:
 					for (const query of branch.children)
-						this.query(query, not);
+						this.query(query);
 						
 					break;
 				case BranchOp.not: 
@@ -47,7 +48,7 @@ namespace Backer.TruthTalk
 					const instance = this.clone();
 					
 					for (const query of branch.children)
-						instance.query(query, not);
+						instance.query(query);
 					
 					const snap = instance.snapshot();
 					this.filter(x => !snap.includes(x));
@@ -60,24 +61,56 @@ namespace Backer.TruthTalk
 					for (const query of branch.children)
 					{
 						const instance = this.clone();	
-						instance.query(query, not);
+						instance.query(query);
 						instances.push(instance);
 					}
 					
 					const snap = instances.flat();
 					this.filter(x => snap.includes(x));
 				}
+				case BranchOp.has:
+					
+					break;
 			}
 		}
 		
-		leaf(leaf: Leaf, not = false) 
+		leaf(leaf: Leaf) 
 		{
 			switch (leaf.op)
 			{
 				case LeafOp.surrogate:
-					this.filter(x => x.is(<PLATypes>leaf));
+					this.filter(x => x[typeOf].parallelRoots.includes((<PLATypes>leaf)[typeOf]));
 					break;
+				case LeafOp.contents:
+					this.contents();
+					break;
+				case LeafOp.roots:
+						this.roots();
+						break;
+				case LeafOp.containers:
+						this.containers();
+						break;
 			}
+		}
+		
+		contents()
+		{
+			this.cursors = new Set(this.snapshot().flatMap(x => x.contents).filter((x): x is PLAObject => !!x));
+		}
+		
+		roots()
+		{
+			this.cursors = new Set(this.snapshot().map(x =>
+				{
+					while (x.parent) 
+						x = x.parent;
+					return x;				
+				}).filter((x): x is PLAObject => !!x));
+		}
+		
+		containers()
+		{
+			this.cursors = new Set(this.snapshot().map(x => x.parent).filter((x): x is PLAObject => !!x));
 		}
 	}
 }
