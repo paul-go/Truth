4ad14ebf0951f9495755f7b59fd53417dcae7ed9
Kaan <kaaninel@gmail.com>
2019-11-10 22:19:18 +0300

    Backer: jest setup

---
+++ Backer/tests/baseline.test.js
@@ -0,0 +1,22 @@
+
+const TruthCode = `
+
+any 
+
+
+
+`;
+
+describe("Compile", () => 
+{
+	beforeAll(async () => 
+	{
+    await page.goto('http://localhost:7007')
+	});
+	
+	it("Truth compiler is still sane (for now)", () => 
+	{
+		
+	});
+	
+});
