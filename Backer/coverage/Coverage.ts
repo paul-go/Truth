2321e3463e988bd0da57a5f7f86cf093e8d90e4c
Kaan <kaaninel@gmail.com>
2019-12-27 13:10:09 +0100

    Backer: Some coverage tests

--- Backer/coverage/Coverage.ts
+++ Backer/coverage/Coverage.ts
@@ -1,8 +1,41 @@
 
-namespace Backer
-{
-	function coverSomething()
+namespace Backer.Tests
+{	
+	async function coverIfTruthCompiles()
 	{
-		return () => true;
+		const doc = await Truth.parse(
+`
+${Util.Headers}
+
+Product
+	Name: string
+	Count: number
+	
+001: Product
+	Name: "Prd1"
+	Count: 4
+
+002: Product
+	Name: "Prd2"
+	Count: 10
+`);
+		doc.program.verify();
+		
+		const faults = Array.from(doc.program.faults.each());
+		
+		return ["A stupid error", () => false, () => faults.length === 0];
+	}
+	
+	async function coverLoadTruthWorks()
+	{
+		console.log("123");
+		console.log("123");
+		console.log("123");
+		console.log("123");
+		console.log("123");
+		console.log("123");
+		console.log("123");
+		console.log("123");
+		console.log("123");
 	}
 }
