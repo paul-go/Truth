3095591b03923be96070c6b95d6db6268baa615b
paulgo <paul@truebase.com>
2019-12-30 09:26:13 -0500

    —

--- Reflex/ReflexML/coverage/SSCompare.ts
+++ Reflex/ReflexML/coverage/SSCompare.ts
@@ -1,14 +1,14 @@
 
 namespace Reflex.ML.Tests
 {
-  async function coverTest()
-  {
-    document.body.append("asdadsa");
-    
-    console.log("123");
-    
-    return [
-      () => void "snapshot()"
-    ]
-  }
+	async function coverTest()
+	{
+		document.body.append("asdadsa");
+		
+		console.log("123");
+		
+		return [
+			() => void "snapshot()"
+		]
+	}
 } 
